5da4b583e963ada05f2935edf9383233c44da2ca
Onurlulardan <onur539688@gmail.com>
2025-04-12 23:48:36 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -42,11 +42,15 @@ export default withAuth(
     pages: {
       signIn: '/auth/login',
     },
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
   }
 );
 
 export const config = {
   matcher: [
+    '/',
     '/dashboard/:path*',
     '/administrations/:path*',
     '/settings/:path*',
